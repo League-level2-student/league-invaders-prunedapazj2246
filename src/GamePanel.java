800eb415d64c86154a37cd8dd0569e9e5f02d49f
root <root@968bbf2caf40>
2020-11-26 03:55:27 +0000

    Finished League Invaders View and Game loop(IP)

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,14 +1,18 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class GamePanel extends JPanel {
-	Font titleFont;
-	public Font(){
-		this.titleFont = new Font("Arial", Font.PLAIN, 48);
-	}
+public class GamePanel extends JPanel implements ActionListener{
+	Timer frameDraw
+	frameDraw = new Timer(1000/60,this);
+	frameDraw.start();
+		Font titleFont = new Font("Arial",Font.PLAIN, 48);
+		Font directionFont = new Font("Arial", Font.PLAIN, 25);
     final int MENU = 0;
     final int GAME = 1;
     final int END = 2;
@@ -23,7 +27,7 @@ public class GamePanel extends JPanel {
 		    drawEndState(g);
 		}
 	}
-	void updateMenuStae() {
+	void updateMenuState() {
 		
 	}
 	void updateGameState(){
@@ -36,6 +40,15 @@ public class GamePanel extends JPanel {
 		
 		g.setColor(Color.BLUE);
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("League Invaders", 50, 150);
+		g.setFont(directionFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("Press ENTER to start", 125, 300);
+		g.setFont(directionFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("Press SPACE for instructions", 75, 450);
 	}
 	void drawGameState(Graphics g) {  
 		g.setColor(Color.BLACK);
@@ -44,6 +57,26 @@ public class GamePanel extends JPanel {
 	void drawEndState(Graphics g)  {  
 		g.setColor(Color.RED);
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.BLACK);
+		g.drawString("Game Over", 50, 150);
+		g.setFont(directionFont);
+		g.setColor(Color.BLACK);
+		g.drawString("You killed 0 enemies", 125, 300);
+		g.setFont(directionFont);
+		g.setColor(Color.BLACK);
+		g.drawString("Press ENTER to restart", 75, 450);
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if(currentState == MENU){
+		    updateMenuState();
+		}else if(currentState == GAME){
+		    updateGameState();
+		}else if(currentState == END){
+		    updateEndState();
+		}	
 	}
 }
 
